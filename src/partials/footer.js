5cb3789f7ee3e531669da6a10b0a4284cb85da98
Harris Beg <legostarwarsguy02@gmail.com>
2019-04-22 12:54:59 -0700

    Updates

--- src/partials/footer.js
+++
@@ -1,6 +0,0 @@
-import React from "react"
-  export default() => (
-    <div className="bg-raven">
-      <h1>To be continued...</h1>
-  </div>
-  )
---
+++ styles-0855bd11dda2384d22a9.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{103:function(n,o,w){},143:function(n,o,w){},144:function(n,o,w){}}]);
+//# sourceMappingURL=styles-0855bd11dda2384d22a9.js.map
